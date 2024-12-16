ca24d8096e9017961bf4098fc03f282a039901da
tkyip3 <tkyip3@gmail.com>
2024-12-16 23:16:33 +0800

    tailwind update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,css,scss}"],
   theme: {
@@ -33,7 +33,7 @@ export default {
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
   daisyui: {
     themes: false,
     darkTheme: "light",
